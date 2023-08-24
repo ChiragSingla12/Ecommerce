ba70166e21df74b26bb4d5566e2cb366b9c35c8e
ChiragSingla12 <Chiragsingla1254@gmail.com>
2023-08-24 22:53:05 +0530

    till controllers code added

---
+++ routes/admin.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+// const rootDir = require('../util/path');
+const productsControllers = require('../controllers/products');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', productsControllers.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsControllers.postAddProduct);
+
+module.exports = router;
