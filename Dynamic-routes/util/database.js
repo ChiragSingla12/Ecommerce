883b20309b114b2e20a134ab23a066db0e6c8531
ChiragSingla12 <Chiragsingla1254@gmail.com>
2023-09-10 23:02:15 +0530

    update delete and get product.

--- Dynamic-routes/util/database.js
+++ Dynamic-routes/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-// schema/ database , username, password, The dialect option specifies which type of database system Sequelize should connect to.
+// schema/database , username, password, The dialect option specifies which type of database system Sequelize should connect to.
 // dialect: 'mysql' indicates that you are configuring Sequelize to connect to a MySQL database.
 
 const sequelize = new Sequelize('node-complete', 'root', 'Yash@2005',{
